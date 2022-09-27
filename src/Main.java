51af58f9d39e650fd1ba862d8ae3da48803f930f
Anastasia Kliuchnikova <anastasia.kluch@gmail.com>
2022-09-27 18:54:37 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 1500;
+        int rubles = 20;
+        int miles = (price / rubles);
+        System.out.println(miles);
+
+
+
+    }
+}
